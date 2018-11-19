e7b0539e75e8378610ec9e4da5f88971c5604846
Ins1eme <insieme1337@gmail.com>
2018-11-19 13:35:09 +0200

    create styleMode service

--- client/src/app/core/services/style-mode.service.ts
+++ client/src/app/core/services/style-mode.service.ts
@@ -10,10 +10,14 @@ export class StyleModeService {
 
     changeStyleMode(styleMode: boolean) {
         this.styleMode.next(styleMode)
+        localStorage.setItem('styleMode', JSON.stringify(styleMode))
     }
 
     getStyleMode() {
+        const styleMode = localStorage.getItem('styleMode')
+        this.styleMode.next(JSON.parse(styleMode))
         return this.styleMode.asObservable()
+        
     }
 
 }
